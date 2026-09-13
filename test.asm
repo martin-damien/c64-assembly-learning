421859112d64506ef2cee28776d413a2f56166c2
martin-damien <168558789+martin-damien@users.noreply.github.com>
2026-09-13 18:33:01 +0200

    Read and write user input

--- test.asm
+++ test.asm
@@ -1,17 +1,33 @@
         * = $1000
 
+.const choice = $1100
+
 .const chrout = $ffd2
+.const chrin = $ffcf
 
-        ldx #0
+        ldx #00
+        ldy #00
 
-loop:
-        lda text,x
+        lda #'?'
+        jsr chrout
+
+read:   jsr chrin
+        sta choice,y
+        iny
+        cmp #$0D
+        bne read
+        
+        lda #$0D
+        jsr chrout
+        lda #'>'
+        jsr chrout
+
+print:  
+        lda choice,x
         beq done
         jsr chrout
         inx
-        bne loop
+        cmp #$0D
+        bne print
 
 done:   rts
-
-text:   .text "HELLO WORLD"
-        .byte 0
